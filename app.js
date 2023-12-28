852dca92cf6adc453396cae4f1d7cf7c51e31970
gary guan <gguan@turo.com>
2023-12-28 14:00:21 -0800

    feat: load local image and add AI prompt

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 require("dotenv").config();
 const OpenAI = require("openai");
+const fs = require("fs");
 
 const openai = new OpenAI({
   apiKey: process.env["OPENAI_API_KEY"], // This is the default and can be omitted
 });
+
+// function to encode image file data to base64 encoded string
+function base64_encode(file) {
+  return fs.readFileSync(file, "base64");
+}
 
 async function main() {
   const response = await openai.chat.completions.create({
@@ -14,18 +20,14 @@ async function main() {
         content: [
           {
             type: "text",
-            text: "What are in these images? Is there any difference between them?",
+            text: "This is error screenshot of toll agency. Only return a result one of following, please select the option. Case 1: Suspended, Case 2: Wrong credential, Case 3: Blank screen, Case 4: Stop in login screen, Case 5: Others",
           },
           {
             type: "image_url",
             image_url: {
-              url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
-            },
-          },
-          {
-            type: "image_url",
-            image_url: {
-              url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+              url: `data:image/png;base64,${base64_encode(
+                "./screenshots/ca-tollroad/res1_loading.png"
+              )}`,
             },
           },
         ],
